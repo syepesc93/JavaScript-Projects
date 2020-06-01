9892b3a12f1aa463eda3607e08f4c354d1ff9506
Santiago Yepes <santiagoyepescarrera@gmail.com>
2020-05-31 23:55:10 -0400

    Adding functionalities: getting inputs

--- budgetApp/app.js
+++ budgetApp/app.js
@@ -13,7 +13,16 @@ var budgetController = (function() { // using immediate function invoke IIFE
 // this is the UI CONTROLLER
 var UIController = (function() {
 
-    // some code for later  
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector('.add__type').value, // will be either 'inc' for income or 'exp' for expense
+                description: document.querySelector('.add__description').value,
+                value: document.querySelector('.add__value').value
+            };
+        }
+
+    };
 
 })();
 
@@ -21,10 +30,11 @@ var UIController = (function() {
 // this is the GLOBAL APP CONTROLLER (to connect modules we use arguments in the IIFE)
 var controller = (function(budgetCtrl, UICtrl) {
 
-    // TO DO List when clicking the button
-    document.querySelector('.add__btn').addEventListener('click', function() {
+    var ctrlAddItem = function() {
 
         // 1. get the filled input data
+        var input = UICtrl.getInput();
+        console.log(input);
 
         // 2. add the item to the budget controller
 
@@ -34,11 +44,16 @@ var controller = (function(budgetCtrl, UICtrl) {
 
         // 5. display the budget on the UI
 
-    });
+    }
 
-    // TO DO List when hitting enter key
-    document.querySelector('.add__btn').addEventListener('keypress', function(event) {
+    // TO DO List when clicking the button
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
 
+    // TO DO List when hitting enter key (keycode: 13)
+    document.addEventListener('keypress', function(event) {
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
     });
 
 
