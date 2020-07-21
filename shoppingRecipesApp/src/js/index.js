262b93a2bd591055076aaca1e19889a10f434654
Santiago Yepes <santiagoyepescarrera@gmail.com>
2020-07-21 16:05:50 -0400

    Adding first files and installing web and development packages

--- shoppingRecipesApp/src/js/index.js
+++ shoppingRecipesApp/src/js/index.js
@@ -1,1 +1,9 @@
+/* 
+First of all we need to install NPM 
+from the terimanl and in your project folder type: npm init
+then install dependencies as follow: 
+    npm install 'package name' --save --> for dependcies
+    npm install 'package name' --save-dev --> for developer tools (packages could be unistall by using the uninstall keyword)
+*/
+
 // Global app controller
