fcbb325e28897a250709eb75b0935ad650ef55d4
Santiago Yepes <santiagoyepescarrera@gmail.com>
2020-08-12 22:50:32 -0400

    Configured Webpack and Babel

--- shoppingRecipesApp/webpack.config.js
+++ shoppingRecipesApp/webpack.config.js
@@ -1,10 +1,34 @@
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/js/index.js',
+    // source file that contain the code (could be more than one)
+    entry: ['babel-polyfill', './src/js/index.js'],
+    // path to save the bundle JS file
     output: {
-        path: path.resolve(__dirname, 'dist/js'),
-        filename: 'bundle.js'
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/bundle.js'
     },
-    mode: 'development'
+    // location of folder to be open with webpack as local hosting (using command: "npm run start")
+    devServer: {
+        contentBase: './dist/'
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+    ],
+    // babel loader 
+    module: {
+        rules: [{
+            // evaluate and convrt all js files
+            test: /\.js$/,
+            // exclude to evaluate and convert js files that are not from out project
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader'
+            }
+        }]
+    }
 };
