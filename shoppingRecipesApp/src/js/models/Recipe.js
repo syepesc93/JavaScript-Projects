d4545cd86c3d9fc188e5266e13e8b24474e056fa
Santiago Yepes <santiagoyepescarrera@gmail.com>
2020-09-01 19:52:38 -0400

    saving progress

--- shoppingRecipesApp/src/js/models/Recipe.js
+++ shoppingRecipesApp/src/js/models/Recipe.js
@@ -31,4 +31,50 @@ export default class Recipe {
     calcServings() {
         this.servings = 4;
     }
+
+    parseIngredients() {
+        const unitsLong = ['tablespoons', 'tablespoon', 'ounce', 'ounces', 'teaspoon', 'teaspoons', 'cups', 'pounds'];
+        const unitsShort = ['tbsp', 'tbsp', 'oz', 'oz', 'tsp', 'tsp', 'cup', 'pound'];
+
+        const newIngredients = this.ingredients.map(el => {
+            // 1. uniform units
+            let ingredient = el.toLowerCase();
+            unitsLong.forEach((unit, i) => {
+                ingredient = ingredient.replace(unit, unitsShort[i]);
+            })
+
+            // 2. remove parentheses
+            ingerdient = ingerdient.replace(/ *\([^)]*\) */g, " ");
+
+            // 3. parse ingredients into count
+            const ingredientsArray = ingredient.split(' ');
+            // look into an array and find the index of an element
+            const unitIndex = ingredientsArray.findIndex(el2 => unitsShort.includes(el2));
+
+            let objIngredients;
+            if (unitIndex > -1) {
+                // there is a unit
+
+            } else if (parseInt(ingredientsArray[0], 10)) {
+                // there is no unit, but first element is a number
+                objIngredients = {
+                    count: parseInt(ingredientsArray[0], 10),
+                    unit: '',
+                    ingredient: ingredientsArray.slice(1).join(' ')
+                }
+
+            } else if (unitIndex === -1) {
+                // there is no unit
+                objIngredients = {
+                    count: 1,
+                    unit: '',
+                    ingredient: ingredient
+                }
+            }
+
+
+            return ingredient;
+        });
+        this.ingredients = newIngredients;
+    }
 }
