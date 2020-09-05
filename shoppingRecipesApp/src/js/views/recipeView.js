4d91b6182ff6c79fc9e0318aacd7894ecaeaa3cf
Santiago Yepes <santiagoyepescarrera@gmail.com>
2020-09-05 14:38:47 -0400

    Added servings increase and decreases

--- shoppingRecipesApp/src/js/views/recipeView.js
+++ shoppingRecipesApp/src/js/views/recipeView.js
@@ -62,12 +62,12 @@ export const renderRecipe = recipe => {
                 <span class="recipe__info-text"> servings</span>
 
                 <div class="recipe__info-buttons">
-                    <button class="btn-tiny">
+                    <button class="btn-tiny btn-decrease">
                         <svg>
                             <use href="img/icons.svg#icon-circle-with-minus"></use>
                         </svg>
                     </button>
-                    <button class="btn-tiny">
+                    <button class="btn-tiny btn-increase">
                         <svg>
                             <use href="img/icons.svg#icon-circle-with-plus"></use>
                         </svg>
@@ -113,4 +113,15 @@ export const renderRecipe = recipe => {
         </div>
     `;
     DOMelements.recipe.insertAdjacentHTML('afterbegin', markUp);
+};
+
+export const updateServingsIngredients = recipe => {
+    // update counts
+    document.querySelector('.recipe__info-data--people').textContent = recipe.servings;
+
+    // update ingredients
+    const countElements = Array.from(document.querySelectorAll('.recipe__count'));
+    countElements.forEach((el, i) => {
+        el.textContent = formatCount(recipe.ingredients[i].count);
+    });
 };
